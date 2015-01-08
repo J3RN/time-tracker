b19613916913de7bbabc6eaf76961605a2f2eaf4
Jonathan Arnett <jonarnett90@gmail.com>
2015-01-08 09:59:11 -0500

    Model relations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
   belongs_to :customer
+  has_many :time_entries
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,2 +1,5 @@
 class TimeEntry < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :user
+  belongs_to :task
 end
