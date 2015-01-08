50c5aecf0917845a280efbfdb4dfce2f3caba6ec
Jonathan Arnett <jonarnett90@gmail.com>
2015-01-08 10:18:57 -0500

    Enforced authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!, except: ['home#index', 'users#sign_in', 'users#sign_up']
+
   protected
 
   def configure_permitted_parameters
