39b8e0cf4192ac475c0b74e57def12e6dbfa1c14
Jonathan Arnett <jonarnett90@gmail.com>
2016-02-11 20:30:19 -0500

    Devise fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, except: ['home#index', 'users#sign_in', 'users#sign_up']
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
     time_entries_path
@@ -12,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :display_name) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :display_name) }
+    devise_parameter_sanitizer.for(:sign_up) << [ :username, :display_name ]
+    devise_parameter_sanitizer.for(:account_update) << [ :username, :display_name ]
   end
 end
