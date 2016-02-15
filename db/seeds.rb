b56669d8967d46460d1f53f1ad5275fa74d61281
Jonathan Arnett <jonarnett90@gmail.com>
2016-02-15 14:37:41 -0500

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-User.create!(username: 'J3RN', display_name: 'J3RN', email: 'j3rn@j3rn.com', password: 'blargblarg')
-
-Task.create!([
-  { task_name: 'Development' },
-  { task_name: 'Management' }])
+User.create!(username: 'J3RN', display_name: 'J3RN',
+             email: 'jonarnett90@gmail.com', password: 'blargblarg')
 
 Customer.create!([
-  { company: 'covermymeds' },
   { company: 'Dell' },
   { company: 'Mozilla' }])
 
 Customer.first.projects.create!([
   { project_name: 'Project 1' },
   { project_name: 'Project 2' }])
+
+Project.first.tasks.create!([
+  { task_name: 'Development' },
+  { task_name: 'Management' }])
