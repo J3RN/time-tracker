563e6c0c321ec3e90d990b479f47fc3f46578fbf
Jonathan Arnett <jonarnett90@gmail.com>
2016-02-17 10:21:56 -0500

    Better display of time elapsed

--- app/helpers/time_entries_helper.rb
+++ app/helpers/time_entries_helper.rb
@@ -1,5 +1,23 @@
 module TimeEntriesHelper
-  def hours_duration(rational)
-    rational.round(2).to_s + " hours"
+  def duration_display(minutes)
+    if minutes > 60
+      hours = minutes / 60
+      minutes = minutes - (hours * 60)
+
+      "#{hours}h#{minutes}m"
+    else
+      "#{minutes}m"
+    end
+  end
+
+  def long_duration_display(minutes)
+    if minutes > 60
+      hours = minutes / 60
+      minutes = minutes - (hours * 60)
+
+      "#{hours} hours, #{minutes} minutes"
+    else
+      "#{minutes} minutes"
+    end
   end
 end
