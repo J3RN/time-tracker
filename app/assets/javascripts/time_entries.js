0e004ba0b07c9f40e5f60c7c41cd4d3ab0060158
Jonathan Arnett <jonarnett90@gmail.com>
2016-02-19 12:38:29 -0500

    Added Select2 for tag choosing, moved to JS

---
+++ app/assets/javascripts/time_entries.js
@@ -0,0 +1,15 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(() => {
+    $('input[name="time_entry[running]"]').change((event) => {
+        const checked = $(event.currentTarget).is(':checked');
+
+        $('#time_entry_duration').val(null);
+        $('#time_entry_duration').prop('disabled', checked);
+
+        $('#time_entry_result').val(null);
+        $('#time_entry_result').prop('disabled', checked);
+    })
+})
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_action :set_tag, only: [:edit, :update, :destroy]
 
   def index
-    @tags = Tag.all
+    @tags = Tag.includes(:tasks)
   end
 
   def new
