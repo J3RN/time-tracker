6e9721cd156557affc5458e7f12813f46fffed72
Jonathan Arnett <jonarnett90@gmail.com>
2016-02-26 14:18:31 -0500

    Made migration reversible

--- db/migrate/20160224131033_add_user_id_to_tags.rb
+++ db/migrate/20160224131033_add_user_id_to_tags.rb
@@ -1,10 +1,14 @@
 class AddUserIdToTags < ActiveRecord::Migration
-  def change
+  def up
     add_column :tags, :user_id, :integer
 
     # Give each tag a user
     Tag.find_each do |tag|
       tag.update(user_id: tag.time_entries.first.try(:user_id) || User.first)
     end
+  end
+
+  def down
+    remove_column :tags, :user_id, :integer
   end
 end
