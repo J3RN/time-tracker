0c962dbbeab0465a25af29cc4cc05c63c6bd9de7
Jonathan Arnett <jonarnett90@gmail.com>
2016-03-04 13:56:28 -0500

    User dependent: :destroy test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  setup do
+    @user = users(:one)
+    @tag = tags(:one)
+    @task = tasks(:one)
+    @time_entry = time_entries(:one)
+  end
+
+  test "does not orphan tags" do
+    @user.save!
+    @tag.save!
+
+    @tag.update(user: @user)
+    @user.destroy
+
+    # Assert that the tag was destroyed
+    assert(@tag.destroyed?, "Tag not destroyed")
+  end
+
+  test "does not orphan tasks" do
+    @user.save!
+    @task.save!
+
+    @task.update(user: @user)
+    @user.destroy
+
+    # Assert that the task was destroyed
+    assert(@task.destroyed?, "Task not destroyed")
+  end
+
+  # test "does not orphan time_entries" do
+  #   @user.save!
+  #   @time_entry.save!
+
+  #   before_time_entries = time_entry.count
+
+  #   @time_entry.update!(user: @user)
+
+  #   @user.destroy
+
+  #   # Assert that the time_entry was destroyed
+  #   assert_equal(time_entry.count, before_time_entries - 1)
   # end
 end
