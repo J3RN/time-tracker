58006ca85307d055506acfc1f976bc73f918d4e9
Jonathan Arnett <jonarnett90@gmail.com>
2016-03-05 20:13:53 -0500

    Bugfix for user dependent tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,35 +12,38 @@ class UserTest < ActiveSupport::TestCase
     @user.save!
     @tag.save!
 
+    before = Tag.count
+
     @tag.update(user: @user)
     @user.destroy
 
-    # Assert that the tag was destroyed
-    assert(@tag.destroyed?, "Tag not destroyed")
+    # Assert that one or more tags was destroyed
+    assert(before > Tag.count, "No tags destroyed")
   end
 
   test "does not orphan tasks" do
     @user.save!
     @task.save!
 
+    before = Task.count
+
     @task.update(user: @user)
     @user.destroy
 
     # Assert that the task was destroyed
-    assert(@task.destroyed?, "Task not destroyed")
+    assert(before > Task.count, "No tasks destroyed")
   end
 
-  # test "does not orphan time_entries" do
-  #   @user.save!
-  #   @time_entry.save!
+  test "does not orphan time_entries" do
+    @user.save!
+    @time_entry.save!
 
-  #   before_time_entries = time_entry.count
+    before = TimeEntry.count
 
-  #   @time_entry.update!(user: @user)
+    @time_entry.update!(user: @user)
+    @user.destroy
 
-  #   @user.destroy
-
-  #   # Assert that the time_entry was destroyed
-  #   assert_equal(time_entry.count, before_time_entries - 1)
-  # end
+    # Assert that the time_entry was destroyed
+    assert(before > TimeEntry.count, "Time Entry not destroyed")
+  end
 end
