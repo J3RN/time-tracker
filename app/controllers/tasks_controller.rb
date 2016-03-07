5801d0b624f316d11e9e56e63f3ff43c6388e7c0
Jonathan Arnett <jonarnett90@gmail.com>
2016-03-07 11:26:43 -0500

    Tags and Tasks responses

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :set_task, except: [ :index, :new, :create ]
   before_action :set_tags, only: [:new, :edit]
   before_action :set_users, only: [:new, :edit]
   before_action ->{ ensure_ownership(@task) }, only: [:edit, :update, :destroy]
+
+  respond_to :html, :json
 
   def index
     @tasks = Task.includes(:tags)
@@ -11,10 +13,16 @@ class TasksController < ApplicationController
 
     @active = @tasks.active
     @archived = @tasks.unscoped.archived.order(archived_at: :desc)
+
+    respond_with(@tasks)
   end
 
   def new
     @task = Task.new
+  end
+
+  def show
+    respond_with(@task)
   end
 
   def edit
