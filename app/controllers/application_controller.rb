572934c94fd4af84c5d31388ffb478bff6d4d666
Jonathan Arnett <jonarnett90@gmail.com>
2016-10-06 14:29:39 -0400

    Switched default landing page to the tasks page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    time_entries_path
+    tasks_path
   end
 
   protected
