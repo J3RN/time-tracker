0baa8d61974900dce3db3a12297ead48af7cfcc9
Jonathan Arnett <jonarnett90@gmail.com>
2016-10-11 15:15:37 -0400

    Moved and refactored time display methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,24 @@ module ApplicationHelper
   def delete_btn(path)
     link_to "\ue020", path, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn-delete"
   end
+
+  def duration_display(minutes)
+    hours = minutes / 60
+    minutes = minutes - (hours * 60)
+
+    minute_string = minutes > 0 ? "#{minutes}m" : ""
+    hour_string = hours > 0 ? "#{hours}h" : ""
+
+    "#{hour_string} #{minute_string}".strip
+  end
+
+  def long_duration_display(minutes)
+    hours = minutes / 60
+    minutes = minutes - (hours * 60)
+
+    minute_string = minutes > 0 ? "#{minutes} #{'minute'.pluralize(minutes)}" : ""
+    hour_string = hours > 0 ? "#{hours} #{'hour'.pluralize(hours)}" : ""
+
+    "#{hour_string} #{minute_string}".strip
+  end
 end
