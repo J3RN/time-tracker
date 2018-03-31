1b00f5a4cfc834e5292b554ccb2fccea0b820ae0
Jonathan Arnett <jonarnett90@gmail.com>
2018-03-30 22:08:20 -0400

    Rename archived to completed
    Which is funny, in retrospect, because org-mode (now a major influence
    on development) uses the term "archived" instead of "completed." In
    any event, this was a massive find-and-replace operation. I'll be
    lucky if nothing major breaks.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116200213) do
+ActiveRecord::Schema.define(version: 20180331020228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,12 @@ ActiveRecord::Schema.define(version: 20161116200213) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "task_name"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "estimate"
-    t.integer  "priority",    default: 1, null: false
+    t.integer  "priority",     default: 1, null: false
     t.integer  "user_id"
-    t.datetime "archived_at"
+    t.datetime "completed_at"
     t.date     "due_date"
     t.date     "start_date"
   end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
   setup do
-    @task = tasks(:unarchived_full)
+    @task = tasks(:uncompleted_full)
     @tag = tags(:one)
     @time_entry = time_entries(:one)
   end
