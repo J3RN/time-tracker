24ff9a6aa268bd62c98263252d7f6bd6dc8d52f0
Jonathan Arnett <jonarnett90@gmail.com>
2018-07-07 22:36:38 -0400

    Modularize tasks index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,12 +13,30 @@ module ApplicationHelper
 
   def duration_display(minutes)
     hours = minutes / 60
-    minutes = minutes - (hours * 60)
+    minutes = minutes % 60
     "#{format('%02d', hours)}:#{format('%02d', minutes)}"
+  end
+
+  def long_duration_display(minutes)
+    hours = minutes / 60
+    minutes = minutes % 60
+
+    hour_string = "hour".pluralize(hours)
+    minute_string = "minute".pluralize(minutes)
+
+    "#{hours} #{hour_string} and #{minutes} #{minute_string}"
   end
 
   def task_with_tag_labels(task)
     task_div = content_tag(:div, task.task_name)
+    tag_spans = task.tags.map do |tag|
+      content_tag(:span, tag.name, class: ["label", "label-info"])
+    end
+    task_div + tag_spans.join.html_safe
+  end
+
+  def span_task_with_tag_labels(task)
+    task_div = content_tag(:span, task.task_name, class: "task-title")
     tag_spans = task.tags.map do |tag|
       content_tag(:span, tag.name, class: ["label", "label-info"])
     end
