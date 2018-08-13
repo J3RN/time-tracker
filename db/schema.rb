74cc5783ba47ef59a92d23e6c91d23589962851f
Sascha Heylik <sascha@heylik.com>
2018-08-13 23:16:10 +0200

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -15,70 +15,70 @@ ActiveRecord::Schema.define(version: 20180331033131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "taggings", force: :cascade do |t|
-    t.integer  "tag_id"
-    t.integer  "task_id"
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
-    t.index ["task_id"], name: "index_taggings_on_task_id", using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["task_id"], name: "index_taggings_on_task_id"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "name",       null: false
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.text     "task_name"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "estimate"
-    t.integer  "priority",     default: 1, null: false
-    t.integer  "user_id"
+  create_table "tasks", id: :serial, force: :cascade do |t|
+    t.text "task_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "estimate"
+    t.integer "priority", default: 1, null: false
+    t.integer "user_id"
     t.datetime "completed_at"
-    t.date     "due_date"
-    t.date     "start_date"
+    t.date "due_date"
+    t.date "start_date"
   end
 
-  create_table "time_entries", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "task_id"
-    t.integer  "duration",   default: 0
+  create_table "time_entries", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.integer "duration", default: 0
     t.datetime "start_time"
-    t.text     "note"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.boolean  "running"
-    t.string   "goal"
-    t.string   "result"
+    t.text "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "running"
+    t.string "goal"
+    t.string "result"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "username"
-    t.text     "display_name"
-    t.boolean  "admin",                  default: false
-    t.string   "confirmation_token"
+    t.text "username"
+    t.text "display_name"
+    t.boolean "admin", default: false
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
